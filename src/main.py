483c9b52aa54a74d2d0fbd5b55e06073f8d5d46a
Gusmiftah12 <gusmif@ilhamxander.web.id>
2025-02-01 02:52:27 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,33 +7,28 @@ from turnstile import turnstile
 
 Settings.set_language("en")
 
-
 app = Sanic("CloudflareTurnstileSolver")
 CORS(app)
 
 @app.get("/")
-async def health_check():
-    return JSONResponse(content={"status": "ok"}, status_code=200)
+async def health_check(request: Request):
+    return json({"status": "ok"}, status=200)
 
 @app.post("/api")
 async def turnstile_endpoint(request: Request):
-	if not request.json:
-		return json({"error": "Missing JSON body"}, status=400)
+    if not request.json:
+        return json({"error": "Missing JSON body"}, status=400)
 
-	# check if json has only url key
-	if "url" not in request.json or len(request.json) != 1:
-		return json(
-			{"error": "Invalid JSON body, pleas provide only a 'url' key"}, status=400
-		)
+    if "url" not in request.json or len(request.json) != 1:
+        return json({"error": "Invalid JSON body, please provide only a 'url' key"}, status=400)
 
-	url = request.json["url"]
+    url = request.json["url"]
 
-	try:
-		token = turnstile(url)
-		return json({"token": token})
-	except Exception as e:
-		return json({"error": str(e)}, status=500)
-
+    try:
+        token = turnstile(url)
+        return json({"token": token})
+    except Exception as e:
+        return json({"error": str(e)}, status=500)
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8000, single_process=True)
+    app.run(host="0.0.0.0", port=8000, single_process=True)
